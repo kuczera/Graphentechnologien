f7686d9852391f593f77f079d58ffaad0baf4239
Andreas Kuczera <andreas.kuczera@adwmainz.de>
2018-08-07 21:38:32 +0200

    pandoc

--- docs/pandoc.sh
+++ docs/pandoc.sh
@@ -1,2 +1,2 @@
 cat *.md > allpages.txt
-pandoc -N --variable mainfont="Palatino" --variable sansfont="Helvetica" --variable monofont="Menlo" --variable fontsize=12pt --variable version=2.0 allpages.txt --toc -o example14.pdf
+pandoc -N --variable mainfont="Palatino" --variable sansfont="Helvetica" --variable monofont="Menlo" --variable fontsize=12pt --variable version=2.0 allpages.txt --toc -o allpages.pdf
