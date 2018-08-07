ca8495f6cf08dcc5815fc1cd943a9126f43bab5c
Andreas Kuczera <andreas.kuczera@adwmainz.de>
2018-08-07 20:53:44 +0200

    pandoc ergänzt

---
+++ pandoc.sh
@@ -0,0 +1,1 @@
+pandoc -N --template=mytemplate.tex --variable mainfont="Palatino" --variable sansfont="Helvetica" --variable monofont="Menlo" --variable fontsize=12pt --variable version=2.0 MANUAL.txt --pdf-engine=xelatex --toc -o example14.pdf
