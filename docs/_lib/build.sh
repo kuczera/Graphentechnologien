93f41faeac556e413ce2a2dc2e913b0505522417
Andreas Kuczera <andreas.kuczera@adwmainz.de>
2019-04-24 13:30:25 +0200

    build-Skript ergänzt von F. Elwert

--- docs/_lib/build.sh
+++ docs/_lib/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 
-SRC="$(dirname "$0")"/..
+SRC="$(realpath "$(dirname "$0")"/..)"
 LIB="$SRC"/_lib
 DEST="$SRC"/downloads
-FILENAMEBASE=Leitfaden_wissenschaftliches_Arbeiten
+FILENAMEBASE=Graphentechnologien
 TEMPLATES="$SRC"/_includes
-BUILDOPTS=(--standalone --toc --toc-depth=2 --number-sections --default-image-extension=png --wrap=none --normalize)
-LATEXOPTS=(--latex-engine=xelatex --variable=documentclass=scrreprt --variable=mainfont='Linux Libertine O' --variable=sansfont='Linux Biolinum O' --variable=colorlinks --no-tex-ligatures)
+BUILDOPTS=(--standalone --toc --toc-depth=2 --number-sections --default-image-extension=png --wrap=none)
+LATEXOPTS=(--pdf-engine=xelatex --variable=documentclass=scrreprt --variable=mainfont='Linux Libertine O' --variable=sansfont='Linux Biolinum O' --variable=colorlinks)
 
 cd "$SRC"/
 
